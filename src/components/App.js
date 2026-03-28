0e9b7700716934170dfecb50b39602431c6e8b35
Deep <diphiwase0987@gmail.com>
2026-03-28 11:29:38 +0530

    some changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import './../styles/App.css';
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserList from "./UserList";
 import UserDetails from "./UserDetails";
 
